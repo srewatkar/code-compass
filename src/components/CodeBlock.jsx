a1b9d06f7e55e0cfcf4c948ccb4e9e1385e942df
Shweta <shweta.rewatkar2@gmail.com>
2026-04-05 22:18:25 -0700

    feat: add CodeBlock component with highlight.js syntax highlighting

---
+++ src/components/CodeBlock.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from 'react'
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import python from 'highlight.js/lib/languages/python'
+import 'highlight.js/styles/atom-one-dark.css'
+import './CodeBlock.css'
+
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('python', python)
+
+const LANG_CONFIG = {
+  javascript: { label: 'JavaScript', color: '#f0db4f' },
+  python: { label: 'Python', color: '#3572A5' },
+}
+
+export default function CodeBlock({ language, code }) {
+  const codeRef = useRef(null)
+  const { label, color } = LANG_CONFIG[language]
+
+  useEffect(() => {
+    if (codeRef.current) {
+      codeRef.current.removeAttribute('data-highlighted')
+      hljs.highlightElement(codeRef.current)
+    }
+  }, [code])
+
+  return (
+    <div className="code-block">
+      <div className="code-block-header">
+        <span className="dot" style={{ background: color }} />
+        <span style={{ color }}>{label}</span>
+      </div>
+      <pre>
+        <code ref={codeRef} className={`language-${language}`}>
+          {code}
+        </code>
+      </pre>
+    </div>
+  )
+}
