4dedd0ae38681c00333d2b823aa0e0d7bd43c78c
Shweta <shweta.rewatkar2@gmail.com>
2026-04-07 18:22:57 -0700

    feat: wire all 17 pattern simulators into AlgoPanel

--- src/components/algo/AlgoPanel.jsx
+++ src/components/algo/AlgoPanel.jsx
@@ -1,7 +1,44 @@
 import { useState } from 'react'
 import { algoPatterns } from '../../data/algoPatterns'
 import AlgoCodeView from './AlgoCodeView'
+import SlidingWindow from './patterns/SlidingWindow'
+import TwoPointers from './patterns/TwoPointers'
+import FastSlowPointers from './patterns/FastSlowPointers'
+import PrefixSum from './patterns/PrefixSum'
+import MonotonicStack from './patterns/MonotonicStack'
+import BFSTemplate from './patterns/BFSTemplate'
+import DFSTemplate from './patterns/DFSTemplate'
+import Backtracking from './patterns/Backtracking'
+import TopologicalSort from './patterns/TopologicalSort'
+import BinarySearch from './patterns/BinarySearch'
+import BinarySearchOnAnswer from './patterns/BinarySearchOnAnswer'
+import DP1DMemo from './patterns/DP1DMemo'
+import DP1DTab from './patterns/DP1DTab'
+import DP2D from './patterns/DP2D'
+import HashMap from './patterns/HashMap'
+import MergeIntervals from './patterns/MergeIntervals'
+import Heap from './patterns/Heap'
 import './AlgoPanel.css'
+
+const PATTERN_SIMULATORS = {
+  'sliding-window': SlidingWindow,
+  'two-pointers': TwoPointers,
+  'fast-slow-pointers': FastSlowPointers,
+  'prefix-sum': PrefixSum,
+  'monotonic-stack': MonotonicStack,
+  'bfs': BFSTemplate,
+  'dfs': DFSTemplate,
+  'backtracking': Backtracking,
+  'topological-sort': TopologicalSort,
+  'binary-search': BinarySearch,
+  'binary-search-on-answer': BinarySearchOnAnswer,
+  'dp-1d-memo': DP1DMemo,
+  'dp-1d-tab': DP1DTab,
+  'dp-2d': DP2D,
+  'hashmap': HashMap,
+  'merge-intervals': MergeIntervals,
+  'heap': Heap,
+}
 
 export default function AlgoPanel() {
   const [selectedId, setSelectedId] = useState('sliding-window')
@@ -64,11 +101,12 @@ export default function AlgoPanel() {
         <div className="algo-content">
           {activeView === 'code'
             ? <AlgoCodeView blocks={selectedPattern.codeBlocks} />
-            : (
-              <div style={{ color: '#94a3b8', fontSize: 13, fontStyle: 'italic', paddingTop: 8 }}>
-                Simulator coming soon for {selectedPattern.title}
-              </div>
-            )
+            : (() => {
+                const SimulatorComponent = PATTERN_SIMULATORS[selectedPattern.id]
+                return SimulatorComponent
+                  ? <SimulatorComponent />
+                  : <div>No simulator yet</div>
+              })()
           }
         </div>
       </div>
